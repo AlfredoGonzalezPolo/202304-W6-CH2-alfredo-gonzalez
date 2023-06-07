5ac6f9e67ecedc2c7bdeffe858c79143ea8d096d
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-06-07 11:58:49 +0200

    Add updateDisplay to hook

--- src/features/phone/hooks/use.phone.ts
+++ src/features/phone/hooks/use.phone.ts
@@ -1,16 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { updateCall } from "../redux/phone.slice";
+import { updateCall, updateDisplay } from "../redux/phone.slice";
 import { AppDispatch, RootState } from "../../../core/store/store";
 
 export function usePhone() {
   const phone = useSelector((state: RootState) => state.phone);
+
   const dispatch: AppDispatch = useDispatch();
+
   const handleUpdateCall = () => {
     dispatch(updateCall());
   };
 
+  const handleUpdateDisplay = (item: string) => {
+    dispatch(updateDisplay(item));
+  };
+
   return {
     handleUpdateCall,
+    handleUpdateDisplay,
     phone,
   };
 }
